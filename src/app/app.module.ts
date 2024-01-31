5be72a1a5d1e02309b54befd2adc5e2213f6a022
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2024-01-31 14:35:59 -0300

    import angular/fire and firebase libarys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
