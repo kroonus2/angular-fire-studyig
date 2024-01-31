cab6cfdea2543e5124cdd6efe7d6cff97ba88583
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2024-01-31 14:35:11 -0300

    create main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Observable } from 'rxjs';
+import { Item } from './interfaces/item';
+import { AngularFireDatabase, SnapshotAction } from '@angular/fire/compat/database';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-fire-project';
+  email = '' as string;
+  password = '' as string;
+
+  formNewName = '' as string;
+  formNewAge = '' as string;
+
+  listRef: any;
+  list: Observable<Item[]>;
+
+  constructor(public auth: AuthService, private dataBase: AngularFireDatabase) {
+    this.listRef = dataBase.list('list');
+    this.list = this.listRef.snapshotChanges().pipe(
+      map((changes: SnapshotAction<Item>[]) =>
+        changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+  }
+
+  addItem() {
+    this.listRef.push({
+      name: this.formNewName,
+      age: this.formNewAge,
+      email: this.auth.user.email,
+    });
+    this.formNewAge = '';
+    this.formNewName = '';
+  }
+
+  deleteItem(key: string) {
+    this.listRef.remove(key);
+  }
 }
