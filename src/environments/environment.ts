facd636824419e2a6d3d6344fef6ab127551aa5c
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2024-01-31 14:37:46 -0300

    create and update envinnments

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyARVstk4C9Yl5GY42QL_AeWzYjEqYAkfqg",
+        authDomain: "angular-fullstack-project.firebaseapp.com",
+        databaseURL: "https://angular-fullstack-project-default-rtdb.firebaseio.com",
+        projectId: "angular-fullstack-project",
+        storageBucket: "angular-fullstack-project.appspot.com",
+        messagingSenderId: "122555835167",
+        appId: "1:122555835167:web:6220a97286805d36e8c62a"
+    }
+};
